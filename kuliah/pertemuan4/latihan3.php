65329c707d6782cf5a8f9b0a7bd3ff2768901cf3
dwiyyl <dwiyulianti327@gmail.com>
2024-03-05 13:12:12 +0700

    mengerjakan tugas

--- kuliah/pertemuan4/latihan3.php
+++ kuliah/pertemuan4/latihan3.php
@@ -2,9 +2,11 @@
 
 function cetak_angka($baris) {
     for ($i = 1; $i <= $baris; $i++) {
-        for ($j = 1; $j <= $1; $j++) {
+        for ($j = 1; $j <= $i; $j++) {
             echo "$i";
         }
         echo "<br>";
     }
 }
+
+echo cetak_angka(5);
